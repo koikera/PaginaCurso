bd826283f42029f8e68ebc45cd5669fbe47ffc78
Koikera <legendar7gamer@hotmail.com>
2024-02-28 16:23:52 -0300

    Ajuste na rota

--- PaginadeVendas/src/app/app.routes.ts
+++ PaginadeVendas/src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { PaginaInicialComponent } from './components/pagina-inicial/pagina-inicial.component';
 
 export const routes: Routes = [
-    {path: '', component: PaginaInicialComponent}
+    {path: '', component: PaginaInicialComponent, pathMatch: 'full'}
 ];
