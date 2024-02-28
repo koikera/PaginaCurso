c28a2191998586af284b70da9cc00024bb1c97c6
Koikera <legendar7gamer@hotmail.com>
2024-02-28 16:35:31 -0300

    teste rota padrao

--- PaginadeVendas/src/app/app.routes.ts
+++ PaginadeVendas/src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { PaginaInicialComponent } from './components/pagina-inicial/pagina-inicial.component';
 
 export const routes: Routes = [
-    {path: '', component: PaginaInicialComponent, pathMatch: 'full'}
+    
+    {path: 'home', component: PaginaInicialComponent}
 ];
